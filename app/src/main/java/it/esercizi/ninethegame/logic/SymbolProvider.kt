375369c026202881c12479ced6ac4d0e2d748527
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-14 16:23:01 +0200

    Create SymbolProvider.kt
    Class that contain the symbols for the game.

---
+++ app/src/main/java/it/esercizi/ninethegame/logic/SymbolProvider.kt
@@ -0,0 +1,17 @@
+package it.esercizi.ninethegame.logic
+
+class SymbolProvider {
+
+        val symbolsNumeric = listOf("1","2","3","4","5","6","7","8","9")
+
+        val symbolsAlpha = listOf("A","B","C","D","E","F","G","H","I")
+
+        val symbolsEmoji = listOf("😄","😍","💗","😍","💗","😍","💗","😍","💗")
+
+        val symbolsBall = listOf("⚽","🏀","🏈","⚾","\uD83E\uDD4E","🎾","🏐","🏉","🎱")
+
+        val symbolsHeart = listOf("❤️","\uD83E\uDDE1","💛","💚","💙","💜","🖤","🤎","🤍")
+
+        val symbolsColoredCircle = listOf("⚪","⚫","🔴","🔵","🟤","🟣","🟢","🟡","🟠")
+
+}
