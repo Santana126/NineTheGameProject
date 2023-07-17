45cf9664e2e8ec680e2a8b57141008a6b9bae5fb
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-17 23:04:40 +0200

    Update GameClass.kt
    Class formatted and loadSymbol method updated.

--- app/src/main/java/it/esercizi/ninethegame/logic/game/GameClass.kt
+++ app/src/main/java/it/esercizi/ninethegame/logic/game/GameClass.kt
@@ -14,22 +14,22 @@ class GameClass : Parcelable {
     val choice = mutableStateOf(1)
 
     @IgnoredOnParcel
-    val squaresSymbol = mutableListOf("","","","","","","","","")
+    val squaresSymbol = mutableListOf("", "", "", "", "", "", "", "", "")
 
     @IgnoredOnParcel
-    val squaresValues = mutableStateListOf("","","","","","","","","")
+    val squaresValues = mutableStateListOf("", "", "", "", "", "", "", "", "")
 
     @IgnoredOnParcel
-    val secretCode = mutableStateListOf("","","","","","","","","")
+    val secretCode = mutableStateListOf("", "", "", "", "", "", "", "", "")
 
     @IgnoredOnParcel
-    val distanceVector = mutableStateListOf("","","","","","","","","")
+    val distanceVector = mutableStateListOf("", "", "", "", "", "", "", "", "")
 
     @IgnoredOnParcel
     val selectedSquareIndex = mutableStateOf(-1)
 
     @IgnoredOnParcel
-    val retrySquaresValue = mutableStateListOf("","","","","","","","","")
+    val retrySquaresValue = mutableStateListOf("", "", "", "", "", "", "", "", "")
 
     @IgnoredOnParcel
     val selectedRetrySquareIndex = mutableStateOf(-1)
@@ -41,59 +41,48 @@ class GameClass : Parcelable {
     val attempt = mutableStateOf(0)
 
     @IgnoredOnParcel
-    val playerCode = mutableStateListOf("","","","","","","","","")
+    val playerCode = mutableStateListOf("", "", "", "", "", "", "", "", "")
 
 
-    fun calculatePlayerCode(){
-        if(this.attempt.value == 0){
+    fun calculatePlayerCode() {
+        if (this.attempt.value == 0) {
             squaresValues.forEachIndexed { index, s ->
                 playerCode[index] = (s.toInt() + 1).toString()
             }
-        }else{
+        } else {
             retrySquaresValue.forEachIndexed { index, s ->
                 playerCode[index] = (s.toInt() + 1).toString()
             }
         }
     }
 
-    fun loadSymbol(){
+    fun loadSymbol() {
 
         val choice = this.choice.value
 
         val symbolProvider = SymbolProvider()
 
-        if(choice == 1){
-            symbolProvider.symbolsNumeric.forEachIndexed { index, s ->
+        when (choice) {
+            1 -> symbolProvider.symbolsNumeric.forEachIndexed { index, s ->
                 squaresSymbol[index] = s
             }
-        }
-        if(choice == 2){
-            symbolProvider.symbolsAlpha.forEachIndexed { index, i ->
+            2 -> symbolProvider.symbolsAlpha.forEachIndexed { index, i ->
                 squaresSymbol[index] = i
             }
-        }
-        if(choice == 3){
-            symbolProvider.symbolsEmoji.forEachIndexed { index, i ->
+            3 -> symbolProvider.symbolsEmoji.forEachIndexed { index, i ->
                 squaresSymbol[index] = i
             }
-        }
-        if(choice == 4){
-            symbolProvider.symbolsBall.forEachIndexed { index, i ->
+            4 -> symbolProvider.symbolsBall.forEachIndexed { index, i ->
                 squaresSymbol[index] = i
             }
-        }
-        if(choice == 5){
-            symbolProvider.symbolsHeart.forEachIndexed { index, i ->
+            5 -> symbolProvider.symbolsHeart.forEachIndexed { index, i ->
                 squaresSymbol[index] = i
             }
-        }
-        if(choice == 6){
-            symbolProvider.symbolsColoredCircle.forEachIndexed { index, i ->
+            6 -> symbolProvider.symbolsColoredCircle.forEachIndexed { index, i ->
                 squaresSymbol[index] = i
             }
         }
     }
-
 
 
 }
