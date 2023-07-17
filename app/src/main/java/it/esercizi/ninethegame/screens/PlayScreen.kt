a73a371d97f5ae9732aeca48aa281df0f2314e89
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-17 15:27:08 +0200

    Update PlayScreen.kt
    Added app settings

--- app/src/main/java/it/esercizi/ninethegame/screens/PlayScreen.kt
+++ app/src/main/java/it/esercizi/ninethegame/screens/PlayScreen.kt
@@ -1,16 +1,20 @@
 package it.esercizi.ninethegame.screens
 
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
-import it.esercizi.ninethegame.logic.PlayLogic
+import it.esercizi.ninethegame.logic.game.PlayLogic
+import it.esercizi.ninethegame.logic.settings.SettingsClass
 import it.esercizi.ninethegame.ui.theme.MyAppTheme
 
 @Composable
-fun PlayPage(navController: NavHostController) {
+fun PlayPage(navController: NavHostController, appSettings: SettingsClass) {
 
-    MyAppTheme {
+    MyAppTheme(backgroundChoice = appSettings.backgroundChoice.value) {
         val playLogic = PlayLogic()
-        playLogic.PlayInit(navController)
-    }
 
+        playLogic.PlayInit(navController, appSettings)
+
+
+    }
 }
