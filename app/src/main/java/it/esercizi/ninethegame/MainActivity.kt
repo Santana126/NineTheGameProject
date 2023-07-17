f472e988042825aabf8528b3a5c2510e1a9d834b
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-17 15:24:29 +0200

    Update MainActivity.kt
    Added app settings

--- app/src/main/java/it/esercizi/ninethegame/MainActivity.kt
+++ app/src/main/java/it/esercizi/ninethegame/MainActivity.kt
@@ -1,20 +1,18 @@
 package it.esercizi.ninethegame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import it.esercizi.ninethegame.screens.HomePage
-import it.esercizi.ninethegame.screens.PlayPage
-import it.esercizi.ninethegame.screens.TrainingPage
+import it.esercizi.ninethegame.logic.settings.SettingsClass
+import it.esercizi.ninethegame.screens.*
 import it.esercizi.ninethegame.ui.theme.MyAppTheme
 
 
@@ -22,17 +20,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
-            MyAppTheme {
+            val appSettings = remember {SettingsClass()}
+            MyAppTheme(backgroundChoice = appSettings.backgroundChoice.value) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ){
                     val navController = rememberNavController()
 
+
+
                     NavHost(navController = navController, startDestination = "main"){
-                        composable("main") { HomePage(navController) }
-                        composable("playPage") { PlayPage(navController)}
-                        composable("trainingPage"){ TrainingPage(navController)}
+                        composable("main") { HomePage(navController,appSettings) }
+                        composable("playPage") { PlayPage(navController,appSettings)}
+                        composable("trainingPage"){ TrainingPage(navController,appSettings)}
+                        composable("statsPage"){ StatsPage(navController,appSettings) }
+                        composable("settingsPage"){ SettingsPage(navController,appSettings) }
                     }
 
                 }
