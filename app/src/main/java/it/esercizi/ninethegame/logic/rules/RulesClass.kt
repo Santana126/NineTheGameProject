b2569644185233b8a40c558d8f069a67c6aa086d
Gianmarco Corsi <gianmarco.corsi29@gmail.com>
2023-07-18 16:23:26 +0200

    Update RulesClass.kt
    Layout updated and rules (text) added

--- app/src/main/java/it/esercizi/ninethegame/logic/rules/RulesClass.kt
+++ app/src/main/java/it/esercizi/ninethegame/logic/rules/RulesClass.kt
@@ -31,25 +31,36 @@ class RulesClass {
                     .width(300.dp)
                     .border(3.dp, color = Color.Black)
                     .padding(10.dp)
-                    .padding(10.dp)
+                    //.padding(10.dp)
             ) {
                 Text(
                     "Rules",
-                    modifier = Modifier.padding(top = 10.dp),
+                    modifier = Modifier.padding(top = 10.dp).weight(0.1f),
                     style = MaterialTheme.typography.h4
                 )
-                Spacer(modifier = Modifier.height(20.dp))
-                LazyColumn {
+                Spacer(modifier = Modifier.height(10.dp))
+                LazyColumn(modifier = Modifier.weight(0.7f).padding(10.dp)) {
 
                     item {
                         Text(
-                            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis scelerisque placerat ornare. Duis ut dignissim augue. Suspendisse efficitur varius lectus eu egestas. Praesent tortor libero, viverra nec nisl et, porttitor laoreet quam. In pharetra dignissim turpis, in sagittis mi semper sed. Praesent placerat hendrerit nunc, at maximus ligula pellentesque vitae. Curabitur massa urna, consequat non arcu dignissim, gravida auctor velit. Nunc fermentum sem magna, eu hendrerit enim convallis a.\n" +
-                                    "\n"
+                            text = "Lo scopo del gioco è quello di indovinare una combinazione segreta di nove simboli.\n"
+                                    + "Inserire nei riquadri una combinazione di simboli tentando di indovinare la posizione di più simboli possibili.\n"
+                                    + "Una volta riempiti i riquadri con i simboli puoi confermare la tua scelta. (Ricorda di inserire tutti i simboli necessari e fai in modo che essi siano diversi tra loro)\n"
+                                    + "Dopo aver confermato il tuo codice, ti verrà mostrata per ogni simbolo, la distanza tra la sua posizione e quella in cui si trova all'interno della combinazione segreta\n"
+                                    + "Attenzione: la distanza può indicare il numero di riquadri sia verso destra che verso sinistra, inoltre se la distanza è maggiore del numero di riquadri disponibili in quella posizione, puoi continuare contando dall'altro lato dei riquadri"
+                                    + "Puoi capire meglio guardando l'esempio qui sotto o giocando il tutorial"
+                                    + "Tramite queste informazioni dovrai capire dove si trovano i simboli nella combinazione segreta, fare le tue scelte e confermare ancora il codice.\n"
+                                    + "Se non ti è possibile indovinare la posizione dei simboli non ti preoccupare. È presente in alto a destra un bottone con cui chiedere un aiuto, esso ti indicherà la posizione corretta di un simbolo e lo inserirà in automatico all'interno della combinazione.\n"
+                                    + "Nella modalità Play avrai un solo tentativo dopo aver visto la distanza tra i simboli e il tuo scopo sarà quello di indovinare la combinazione nel minor tempo possibile.\n"
+                                    + "Nella modalità Training potrai allenarti e la partità finirà quando avrai indovinato la combinazione segreta. Non avrai un limite di tentativi, perciò qui il tuo scopo è quello di impiegare il minor numero di tentetivi."
                         )
                     }
+                    item {
+                        Text(text = "Nella barra in basso è presente la pagina delle impostazioni dove puoi modificare alcuni aspetti del gioco, e la pagina dedicata ai feedback dove puoi comunicare problemi o miglioramenti sull'applicazione\n")
+                    }
                 }
-                Spacer(modifier = Modifier.weight(1f))
-                Row() {
+                //Spacer(modifier = Modifier.weight(1f))
+                Row(modifier = Modifier.background(color = Color.DarkGray).weight(0.1f)) {
                     Button(onClick = onClose, modifier = Modifier.padding(5.dp)) {
                         Text("Play the Tutorial")
                     }
